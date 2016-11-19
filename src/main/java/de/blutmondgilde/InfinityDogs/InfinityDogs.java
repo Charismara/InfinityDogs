c34608131b0416051775fd8527ced32f83804c3d
Charismara <buck.tim6@googlemail.com>
2016-11-19 22:22:19 +0100

    Default edit #01

--- src/main/java/de/blutmondgilde/InfinityDogs/InfinityDogs.java
+++ src/main/java/de/blutmondgilde/InfinityDogs/InfinityDogs.java
@@ -5,10 +5,11 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = InfinityDogs.MODID, version = InfinityDogs.VERSION)
+@Mod(modid = InfinityDogs.MODID, version = InfinityDogs.VERSION, name = InfinityDogs.NAME)
 public class InfinityDogs
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "infinitydogs";
+    public static final String NAME = "InfinityDogs";
     public static final String VERSION = "1.0";
     
     @EventHandler
