843fd4062678e5db88323cf07a75bebe3a5cabbf
Charismara <buck.tim6@googlemail.com>
2016-11-19 23:33:49 +0100

    Default edit #03

--- src/main/java/de/blutmondgilde/InfinityDogs/InfinityDogs.java
+++ src/main/java/de/blutmondgilde/InfinityDogs/InfinityDogs.java
@@ -5,6 +5,8 @@ import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = InfinityDogs.MODID, version = InfinityDogs.VERSION, name = InfinityDogs.NAME)
 public class InfinityDogs {
@@ -13,7 +15,14 @@ public class InfinityDogs {
 	public static final String VERSION = "1.0";
 
 	@EventHandler
+	public void preinit(FMLPreInitializationEvent event) {
+	}
+	
+	@EventHandler
 	public void init(FMLInitializationEvent event) {
-
+	}
+	
+	@EventHandler
+	public void postinit(FMLPostInitializationEvent event) {
 	}
 }
