39ef16f1341176d0fb8ca71853597b270e8621ca
Julian Schmid <Julschmid1@gmail.com>
2016-11-19 23:30:40 +0100

    Default edit #02

--- src/main/java/de/blutmondgilde/InfinityDogs/InfinityDogs.java
+++ src/main/java/de/blutmondgilde/InfinityDogs/InfinityDogs.java
@@ -7,15 +7,13 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = InfinityDogs.MODID, version = InfinityDogs.VERSION, name = InfinityDogs.NAME)
-public class InfinityDogs
-{
-    public static final String MODID = "infinitydogs";
-    public static final String NAME = "InfinityDogs";
-    public static final String VERSION = "1.0";
-    
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
-    }
+public class InfinityDogs {
+	public static final String MODID = "infinitydogs";
+	public static final String NAME = "InfinityDogs";
+	public static final String VERSION = "1.0";
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+
+	}
 }
